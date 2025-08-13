b2d831f6f8e3fc1d4703f0a5c165b25c5f8362ba
Dimas Vico Satria Nugraha <dimasvicosatrianugraha@gmail.com>
2025-08-13 18:05:20 +0700

    feat: adding a settings but not yet functional by (dimas)

--- lib/header.dart
+++ lib/header.dart
@@ -16,6 +16,50 @@ class Header extends StatelessWidget implements PreferredSizeWidget {
       ),
       centerTitle: true,
       backgroundColor: const Color(0xFF4A4877),
+      actions: [
+        Padding(
+          padding: EdgeInsets.only(right: 10),
+          child: IconButton(
+            onPressed: () {
+              showModalBottomSheet(
+                context: context,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+                ),
+                builder: (context) {
+                  return Padding(
+                    padding: EdgeInsets.all(16),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Pengaturan',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        ListTile(
+                          leading: Icon(Icons.palette),
+                          title: Text('Ganti Tema'),
+                          onTap: () {},
+                        ),
+                        ListTile(
+                          leading: Icon(Icons.access_time),
+                          title: Text('Atur Jam Default'),
+                          onTap: () {},
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              );
+            },
+            icon: Icon(Icons.settings, color: Colors.white),
+          ),
+        ),
+      ],
       shape: const RoundedRectangleBorder(
         borderRadius: BorderRadius.vertical(bottom: Radius.circular(30)),
       ),
